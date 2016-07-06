92a6aacb032081b15f4e9a20973820c9a073c6e0
steftr <steftrenson@gmail.com>
2016-07-06 11:33:25 +0200

    Showing a user in the most simple way imaginable

--- angular2/project/app/components/details.component.ts
+++ angular2/project/app/components/details.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
+// import { ROUTER_DIRECTIVES } from '@angular/router';
+import { User } from './user-model';
 
 @Component({
   selector: 'my-dummy',
-  template: '<div>Testert</div>'
+  templateUrl: 'app/components/user.component.html'
 })
 export class DetailsComponent {
 
+    user: User;
+    
 	constructor() {
-
+        this.user = new User;
+        this.user.firstName = 'Yorick';
+        this.user.id = 1;
+        this.user.lastName = 'Horrie';
 	}
 }
